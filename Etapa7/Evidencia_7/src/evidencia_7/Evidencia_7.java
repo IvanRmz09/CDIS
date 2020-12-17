326bb7d9af7f64ac7cc262b307b6b89184adb2ca
Ivan Ramirez <a01197409@itesm.mx>
2020-12-16 21:52:35 -0600

    evidencia7

---
+++ Etapa7/Evidencia_7/src/evidencia_7/Evidencia_7.java
@@ -0,0 +1,12 @@
+package evidencia_7;
+
+/**
+ *
+ * @author ivanr
+ */
+public class Evidencia_7 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
